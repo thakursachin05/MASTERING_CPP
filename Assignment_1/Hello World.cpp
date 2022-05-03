16b78bb440d1feb91694a1fd5c7b23d6e89a2d95
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-05-03 13:14:26 +0530

    HackerRank Hello word program with question link

---
+++ Assignment_1/Hello World.cpp
@@ -0,0 +1,11 @@
+//Objective
+// This is a simple challenge to help you practice printing to stdout. You may also want to complete Solve Me First in C++ before attempting this challenge.
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
