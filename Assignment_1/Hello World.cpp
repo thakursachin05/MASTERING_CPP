8fb9cf44be393f3986ee8cd8b6eaecda3ea9720c
Devendra Singh Rana <60600782+thakursachin05@users.noreply.github.com>
2022-05-03 13:18:38 +0530

    HackerRank Hello world program with question link

--- Assignment_1/Hello World.cpp
+++ Assignment_1/Hello World.cpp
@@ -1,5 +1,6 @@
 //Objective
 // This is a simple challenge to help you practice printing to stdout. You may also want to complete Solve Me First in C++ before attempting this challenge.
+// https://www.hackerrank.com/challenges/cpp-hello-world/problem?isFullScreen=true
 
 #include <iostream>
 #include <cstdio>
