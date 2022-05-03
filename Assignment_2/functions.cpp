911da204e1755635222cbfcc46d2d4e1fe20f135
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-05-03 14:00:04 +0530

    HackerRank Functions to find max of 4 int program with question link

---
+++ Assignment_2/functions.cpp
@@ -0,0 +1,37 @@
+// https://www.hackerrank.com/challenges/c-tutorial-functions/problem?isFullScreen=true
+// Function to min max of four numbers
+
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+
+int max_of_four(int a,int b, int c,int d){
+    int max = 0;
+    if(max<a){
+        max = a;
+    }
+    if(max<b){
+        max = b;
+    }
+    if(max<c){
+        max = c;
+    }
+    if(max<d){
+        max = d;
+    }
+    return max;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
