b616091452d2449398f23bce0ca42047edccd80a
Devendra Singh Rana <60600782+thakursachin05@users.noreply.github.com>
2022-05-05 10:33:58 +0530

    Update Array_Print_reverse.cpp

--- Assignment_2/Array_Print_reverse.cpp
+++ Assignment_2/Array_Print_reverse.cpp
@@ -11,20 +11,34 @@
 #include <algorithm>
 using namespace std;
 
-void printReverse(int arr[],int i,int n){
-    if(i==n) return;
-    printReverse(arr,i+1,n);
-    cout<<arr[i]<<" ";
+// void printReverse(int arr[],int i,int n){
+//     if(i==n) return;
+//     printReverse(arr,i+1,n);
+//     cout<<arr[i]<<" ";
+// }
+
+void reverseArr(int *arr,int n){
+    inn s = 0;
+    int e = n-1;
+    while (s<e)
+    {
+        swap(arr[s],arr[e]);
+    }
+    
+
 }
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
     int n;
     cin>>n;
-    int a[n];
+    int arr[n];
     for(int i=0;i<n;i++){
-        cin>>a[i];
+        cin>>arr[i];
     }
-    printReverse(a,0,n);
+    reverseArr(arr,n);
+     for(auto x : arr){
+        cout<<x<<<" ";
+    }
     return 0;
 }
