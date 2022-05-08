bdba3ddbebaac893418b03b141eaabe9edb811d2
Devendra Singh Rana <60600782+thakursachin05@users.noreply.github.com>
2022-05-08 16:06:06 +0530

    Update 2_key_keyBoardDP.cpp

--- Assignment_5/2_key_keyBoardDP.cpp
+++ Assignment_5/2_key_keyBoardDP.cpp
@@ -1,4 +1,4 @@
-//https://leetcode.com/problems/2-keys-keyboard/submissions/
+//https://leetcode.com/problems/2-keys-keyboard/
 //LeetCode Q.No. - 650. 2 Keys Keyboard
 
 // This time we have implemented the dp approach to the solve this question for improving the time complexity for larger test cases
@@ -18,24 +18,23 @@ void primefactors(){
         }
     }
 }
-
-class Solution {
-public:
-    
-    
-    int helper(int n){
-        if(n<2) return 0;
-        vector<int> dp(n+1,0);
+vector<int> dp;
+void helper(){
+        primefactors();
+        int n = 1e3+5;
+        for(int i=0;i<n;i++){
+            dp.push_back(0);
+         }
+        
         dp[2]=2;
         for(int i=3;i<n+1;i++){
             dp[i] =  primeFactor[i] + dp[i/primeFactor[i]];
         }
-        
-        return dp[n];
     }
-    
+class Solution {
+public: 
     int minSteps(int n) {
-        if(primeFactor.size()==0) primefactors();
-        return helper(n);
+        if(dp.size()==0) helper();
+        return dp[n];
     }
 };
