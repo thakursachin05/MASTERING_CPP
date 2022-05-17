6c1c67416c56f717eda737cc25fb83fcf40cd249
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-05-17 12:36:21 +0530

    Create firstBadVersion.cpp

---
+++ Assignment_11/firstBadVersion.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/first-bad-version/
+// 278. First Bad Version
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+         int low = 1;
+        int high = n;
+        int ans = 0;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(isBadVersion(mid)){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+};
