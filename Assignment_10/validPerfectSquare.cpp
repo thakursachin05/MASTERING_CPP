163c98724934c4141ca9dc72b02106b68852f93c
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-05-17 12:36:02 +0530

    Create validPerfectSquare.cpp

---
+++ Assignment_10/validPerfectSquare.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/valid-perfect-square/
+// 367. Valid Perfect Square
+
+
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if(num==1) return true;
+        long long low = 0;
+        long long high = num;
+        while(high-low>1){
+            long long mid = low + (high-low)/2;
+            if(mid*mid==num) return true;
+            else if(mid*mid>num) high = mid;
+            else low = mid;
+        }
+        return false;
+    }
+};
