232a44ef2ba7a6dbffc7fe5f81b174d05f644967
Devendra Singh Rana <60600782+thakursachin05@users.noreply.github.com>
2022-06-06 20:02:36 +0530

    Update mergeTwoSortedList.cpp

--- Assignment_13/mergeTwoSortedList.cpp
+++ Assignment_13/mergeTwoSortedList.cpp
@@ -1,1 +1,38 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* i = list1;
+        ListNode* j = list2;
+        ListNode* temp = new ListNode(-1);
+        ListNode* k = temp;
+        
+        while(i!=nullptr && j!=nullptr){
+            if(i->val >= j->val){
+                k->next = j;
+                j = j->next;
+            }
+            else{
+                k->next = i;
+                i = i->next;
+            }
+               k = k->next;
+        }
+        if(i!=nullptr){
+            k->next = i;
+        }
+        if(j!=nullptr){
+            k->next = j;
+        }
+        return temp->next;
+    }
+};
