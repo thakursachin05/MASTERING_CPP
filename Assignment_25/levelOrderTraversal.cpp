0e9624d786be4f37717e9dde3a7638d9df998487
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-07-10 20:24:12 +0530

    Create levelOrderTraversal.cpp

---
+++ Assignment_25/levelOrderTraversal.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+	
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+                }
+
+               return root;
+           }
+        }
+/*
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+*/
+
+    void levelOrder(Node * root) {
+        /// BOOM CODE IS HERE ///
+
+        if(!root) return;
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty()){
+            Node* n = q.front();
+            q.pop();
+            cout<<n->data<<" ";
+            if(n->left) q.push(n->left);
+            if(n->right) q.push(n->right);
+            
+        }
+    }
+
+}; //End of Solution
