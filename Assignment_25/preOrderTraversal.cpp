1b6324f192221f0e00991bf86337ccc69a44f7ef
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-07-10 20:24:19 +0530

    Create preOrderTraversal.cpp

---
+++ Assignment_25/preOrderTraversal.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void happyDebug(TreeNode* root,vector<int> &ans){
+        if(root==nullptr) return;
+        
+        ans.push_back(root->val);  
+        happyDebug(root->left,ans);
+        happyDebug(root->right,ans);
+        
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        happyDebug(root,ans);
+        return ans;
+    }
+};
