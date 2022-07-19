29e1a38a4587ac6c609b6befca3c302849068a63
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-07-19 22:44:54 +0530

    Create minStepsInInfiniteGrid.cpp

---
+++ Assignment_24/minStepsInInfiniteGrid.cpp
@@ -0,0 +1,7 @@
+int Solution::coverPoints(vector<int> &A, vector<int> &B) {
+    int totalMoves = 0;
+    for(int i=0;i<A.size()-1;i++){
+        totalMoves += max(abs(A[i]-A[i+1]),abs(B[i]-B[i+1]));
+    }
+    return totalMoves;
+}
