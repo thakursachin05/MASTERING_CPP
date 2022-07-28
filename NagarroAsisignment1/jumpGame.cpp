c3e5180c91e4da45e81c0e33287d66f458b7c937
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-07-28 17:20:22 +0530

    Create jumpGame.cpp

---
+++ NagarroAsisignment1/jumpGame.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int reachable = 0;
+        
+        for(int i=0;i<nums.size();i++){
+            if(reachable < i) return false;
+            reachable = max(reachable,nums[i]+i);
+        }
+        
+        return true;
+    }
+};
