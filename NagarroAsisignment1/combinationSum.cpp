d84395bb74ca485bb8886b16ec2d4113d7e34b5e
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-07-28 17:20:12 +0530

    Create combinationSum.cpp

---
+++ NagarroAsisignment1/combinationSum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void f(int i,int target,vector<int> &ans,vector<vector<int>> &res,vector<int> &arr){
+        if(i==arr.size()){
+            if(target==0){
+                res.push_back(ans);
+                return ;
+            }
+            return;
+        }
+        
+        if(target >= arr[i]){
+            ans.push_back(arr[i]);
+            f(i,target-arr[i],ans,res,arr);
+            ans.pop_back();
+        }
+        
+        f(i+1,target,ans,res,arr);
+    }
+    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
+        sort(arr.begin(),arr.end());
+        vector<int> ans;
+        vector<vector<int>> res;
+        
+        f(0,target,ans,res,arr);
+        return res;
+    }
+};
