bfe12fa5cf4093ea9714e5af7c58371c247070a4
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-08-15 21:49:33 +0530

    Create rotateArray.cpp

---
+++ Assignment_35/rotateArray.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k = k%nums.size();
+        int idx1 = nums.size()-k;
+        reverse(nums.begin(),nums.begin()+idx1);
+        reverse(nums.begin()+idx1,nums.end());
+        reverse(nums.begin(),nums.end());
+    }
+};
