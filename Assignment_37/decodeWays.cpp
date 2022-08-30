eae6752a059d1e186735a241ca2eea2d1b520871
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-08-30 11:52:00 +0530

    Create decodeWays.cpp

---
+++ Assignment_37/decodeWays.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int f(int i,string &s,vector<int> &dp){
+        if(i==s.size()){
+            return 1;
+        }
+        if(dp[i]!=-1) return dp[i];
+        int ans = 0;
+        if(s[i] != '0'){
+            ans = f(i+1,s,dp);
+        }
+        if(i<s.size()-1 && (s[i]=='1' || (s[i]=='2' and s[i+1] <='6'))){
+            ans += f(i+2,s,dp);                                                                
+        }
+        return dp[i] = ans;
+    }
+    int numDecodings(string s) {
+        vector<int> dp(s.size()+1,-1);
+        return f(0,s,dp);
+    }
+};
