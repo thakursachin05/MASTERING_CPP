6a6d5bbc540b8c8c699e26ac56ff4c7ab0acea29
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-08-30 11:52:05 +0530

    Create LongestIncreasingSubs.cpp

---
+++ Assignment_37/LongestIncreasingSubs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int f(int curr,int prev,vector<int> &nums,vector<vector<int>> &dp){
+        if(curr==nums.size()){
+            return 0;
+        }
+        if(dp[curr][prev+1]!=-1) return dp[curr][prev+1];
+        
+        if(prev==-1 || nums[curr]>nums[prev]){
+            return dp[curr][prev+1] = max(1+f(curr+1,curr,nums,dp),f(curr+1,prev,nums,dp));
+        }
+        return dp[curr][prev+1] = f(curr+1,prev,nums,dp);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        vector<vector<int>> dp(nums.size()+3,vector<int>(nums.size()+3,-1));
+        return f(0,-1,nums,dp);
+    }
+};
