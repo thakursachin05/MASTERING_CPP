533bb9d18516d19f79225905b3d80e81e337686b
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-11-17 21:50:02 +0530

    Create insertionSort1.cpp

---
+++ Assignment_07/insertionSort1.cpp
@@ -0,0 +1,100 @@
+// https://www.hackerrank.com/challenges/insertionsort1/problem?isFullScreen=true
+// HackerRank Problem Insertion sort 1
+
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'insertionSort1' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY arr
+ */
+void print(vector<int> arr){
+    for(auto x : arr) cout<<x<<" ";
+    cout<<endl;
+}
+void insertionSort1(int n, vector<int> arr) {
+    int key = arr[n-1];
+    int i = n-1;
+    int j = n-2;
+    while(j>=0 && arr[j]>key){
+        arr[j+1] = arr[j];
+        j--;
+        print(arr);
+    }
+    arr[j+1] = key;
+    print(arr);    
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    insertionSort1(n, arr);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
