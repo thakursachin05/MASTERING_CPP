ee66a588be9e3515eb96e7eea6f58405cf6d9ccc
Devendra Singh Rana <60600782+thakursachin05@users.noreply.github.com>
2022-12-18 22:08:02 +0530

    Update minimum_distances.cpp

--- Assignment_8_&_9/minimum_distances.cpp
+++ Assignment_8_&_9/minimum_distances.cpp
@@ -10,12 +10,6 @@ string ltrim(const string &);
 string rtrim(const string &);
 vector<string> split(const string &);
 
-/*
- * Complete the 'minimumDistances' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts INTEGER_ARRAY a as parameter.
- */
 
 int minimumDistances(vector<int> a) {
     int minDistance = INT_MAX;
